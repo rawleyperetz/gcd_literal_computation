56d3bc667a1cca56cfda851d7edea84e90f2bd8d
rawleyperetz <70651978+rawleyperetz@users.noreply.github.com>
2022-08-09 23:00:13 +0200

    Update gcd_first.py

--- gcd_first.py
+++ gcd_first.py
@@ -13,7 +13,7 @@ def gcd_val(firstnumber, secondnumber):
     print(firstnumber, '=', secondnumber, '*', quotient, '+', remainder)
     r = remainder
     j = 1
-    while r > 1:
+    while r > 0:
         a = number[j]
         b = number[j+1]
         q = math.floor(a/b)
@@ -26,38 +26,5 @@ def gcd_val(firstnumber, secondnumber):
             j = j + 1
 
 print(gcd_val(30,7))
+print("===============")
 print(gcd_val(7,30))
-#into an array because we will need it to iterate
-'''number = []
-
-#the two number needed
-firstnumber = int(input("Enter the first number: "))
-secondnumber = int(input("Enter the second number: "))
-
-#numbers are added to the number array
-number.append(firstnumber)
-number.append(secondnumber)
-
-#returns the quotient
-quotient = math.floor(firstnumber/secondnumber)
-
-#returns the remainder and appends the remainder
-remainder = firstnumber - (secondnumber * quotient)
-number.append(remainder)
-
-print(firstnumber, '=', secondnumber, '*', quotient, '+', remainder)
-
-r = remainder
-j = 1
-while r > 0:
-    a = number[j]
-    b = number[j+1]
-    q = math.floor(a/b)
-    if q == 0:
-        break
-    else:
-        r = a - (b*q)
-        number.append(r)
-        print(a, '=', b, '*', q, '+', r)
-        j = j + 1'''
-    
